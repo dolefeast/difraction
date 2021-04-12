d637ab9a0513aced45a6db28497ff25d64f53a47
dolefeast <santigu10@hotmail.com>
2021-04-12 16:35:11 +0200

    Started using classes to define an experiment. Doesnt recognize changes in Z, though the rest seems to be working fine

--- fourier.py
+++ fourier.py
@@ -2,49 +2,109 @@ import matplotlib.pyplot as plt
 import scipy.fft as fft
 import numpy as np
 
-N = 200
-height = 80 
-width = 80
+
+class Monochromatic_Experiment:
+    def __init__(self, wavelength, N, size):
+        self.N = N #Number of pixels in the slit
+        self.size = size #size of the slit, in mm
+        self.wavelength = wavelength #Wavelength of the incoming wave
+
+        #Coordinate space coordinates
+        self.x = np.linspace(-size/2, size/2, N)
+        self.y = np.linspace(-size/2, size/2, N)
+        self.X, Y = np.meshgrid(self.x, self.y)
+
+        #Frequency space coordinates
+        kx = np.linspace(-2*np.pi * N, -2*np.pi, N)
+        ky = np.linspace(-2*np.pi * N, -2*np.pi, N)
+        self.Kx, self.Ky = np.meshgrid(kx, ky)
+
+        self.slit = np.zeros((N, N))
+
+    def rectangle(self, width, height, x0, y0):
+        """Adds a rectangular slit to the setup. All units in px.
+        Params:
+        width: int
+            Width in pixels of the rectangular slit
+        height: int
+            Height in pixels of the rectangular slit
+        x0: int
+            Horizontal position of the center of the recangular slit
+        y0: int
+            Vertical position of the center of the recangular slit
+        """
+        self.slit[x0 - height//2:x0 + height//2, y0 - width//2:y0 + width//2] += 1
+
+    def propagate(self, Z, A):
+        """
+        Params:
+        Z: The distance from the screen to the slit
+        A: The Fourier transform A(kx, ky, 0)
+
+        Returns the forier transform A(kx, ky, Z)
+        """
+        phase = np.exp(1j*Z*np.sqrt( (2*np.pi/self.wavelength)**2 - self.Kx**2 - self.Ky**2)) #Not 100% sure of this
+        return A*phase
+
+    def fourier_transform(self):
+        """Returns the fourier transform of the slit.
+        """
+        fourier = fft.fft2(self.slit)
+        return fft.fftshift(fourier)
+
+    def intensity(self, F):
+        """Returns the intensity of a field F
+        Params:
+        F: float
+        """
+        return np.real(F*np.conjugate(F))
+
+    def field_at_Z(self, Z):
+        """Returns the interference pattern observed at a distance Z from the slit. Kind of a 'full experiment'
+        Params:
+        Z: float
+        Distance, in mm, from the slit to the 'screen'
+        Returns:
+        self.N x self.N array of intensities to be plotted"""
+
+        fourier_at_0 = self.fourier_transform()
+        fourier_at_z = fft.fftshift(self.propagate(Z, fourier_at_0))
+        field = fft.ifft2(fourier_at_z)
+
+        return self.intensity(field)
+
+
+
+
+
+#Units definition
+mm = 1
+m = 1000*mm
+cm = 10*m
+nm = 1e-6 * mm
+
+N = 200 # pixels number
+size = 500 * mm # physical length of a square (extent_x = extent_y)
+
+
+wavelength = 600*nm
+Z = 1*m
+
+exp = Monochromatic_Experiment(wavelength, N, size)
+exp.rectangle(30, 30, 100, 100)
+
+
+
+
 fig, ax = plt.subplots(1, 2)
 
-m = 1
-cm = m/10
-mm = cm/10
-nm = 1e-9
-
-l_ambda = 0.0001
-Z = 40
-h=1
-A=5
-
-xfig = np.zeros((N, N))
-
-
-xfig[(N-height)//2:(N+height)//2, (N-width)//2:(N+width)//2] += 1
-
-ax[0].imshow(xfig)
-
-X = np.arange(0, N, 1)
-Y = np.arange(0, N, 1)
-U, V = np.meshgrid(X, Y)
-
-yfig = fft.fft2(xfig)
-yfigshift = fft.fftshift(yfig)
-
-for Z in range(200):
-    if Z == 40:
-        print('yiaier')
-    phase = np.exp(2*np.pi*1j*(Z/h)*np.sqrt((1/l_ambda)**2-(U/A)**2-(V/A)**2))#print(phase)
-
-    field = yfigshift*phase
-    #print(field[0,0], yfigshift[0,0])
-
-    U = fft.ifft2(yfigshift*phase)
-
+ax[0].imshow(exp.slit)
+for Z in range(100):
+    print(Z)
     ax[1].cla()
-    ax[1].imshow(np.real(np.conjugate(U)*U))
-    plt.pause(0.001)
-plt.savefig('img.pdf')
+    ax[1].imshow(exp.field_at_Z(50))
+    plt.pause(0.01)
+#plt.savefig('img.pdf')
 plt.show()
 
 
