586c0b800a8cc2a132f25b6ded87b0b32a5625e3
dolefeast <santigu10@hotmail.com>
2021-04-14 22:08:57 +0200

    Added fresnel zones I thinsk its working propoerly

--- fourier.py
+++ fourier.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import scipy.fft as fft
 import numpy as np
 
+#print(FuncAnimation.__doc__)
 
 class Monochromatic_Experiment:
     def __init__(self, wavelength, N, size):
         self.N = N #Number of pixels in the slit
-        self.size = size #size of the slit, in mm
+        self.size = size #size of the experiment, in mm
         self.wavelength = wavelength #Wavelength of the incoming wave
 
         #Coordinate space coordinates
@@ -24,8 +26,9 @@ class Monochromatic_Experiment:
     def spherical_wave(self, r0):
         """In case the wave comes from a source at a distance r0<\inf
         """
-        E = np.exp(1j * 2 * np.pi/self.wavelength * np.sqrt(self.X**2 + self.Y**2+ r0**2))
-        self.slit *= E
+        d = np.sqrt(self.X**2 + self.Y**2+ r0**2) #The distance to the source of every point in the slit (matrix)
+        E = np.exp(1j * 2 * np.pi/self.wavelength * d)/d #The electric field (complex) of every point in the slit (matrix)
+        self.slit *= E 
 
     def rectangle(self, width, height, x0, y0):
         """Adds a rectangular slit to the setup. All units in mm.
@@ -35,19 +38,18 @@ class Monochromatic_Experiment:
         height: int
             Height in mm of the rectangular slit
         x0: int
-            Horizontal position of the center of the recangular slit
+            Horizontal position of the center of the rectangular slit
         y0: int
-            Vertical position of the center of the recangular slit
+            Vertical position of the center of the rectangular slit
         """
         #second try: scaling
-        self.scale_factor = self.N//self.size
+        self.scale_factor = self.N//self.size #To convert from mm to px
         x0 *= self.scale_factor
         y0 *= self.scale_factor
         height *= self.scale_factor
         width *= self.scale_factor
-        self.slit[x0 - height//2:x0 + height//2, int(y0 - width//2):int(y0 + width//2)] = 1+0j 
+        self.slit[int(x0 - height//2):int(x0 + height//2), int(y0 - width//2):int(y0 + width//2)] = 1+0j 
         
-        #self.slit[x0 - height//2:x0 + height//2, y0 - width//2:y0 + width//2] = 1+0j 
 
     def grid(self, width, height, x0, y0, N, offset):
         for n in range(N):
@@ -55,6 +57,24 @@ class Monochromatic_Experiment:
             exp.rectangle(width, height, self.N//2, self.N//2 + n*offset)
             exp.rectangle(height, width, self.N//2 - n*offset, self.N//2 )
             exp.rectangle(height, width, self.N//2 + n*offset, self.N//2 )
+
+    def fresnel_rings(self, Z, maxrings):
+        """Creates a filter given by the fresnel rings. 
+        Params: 
+        Z: float
+            Length at which the focus is to be found
+        """
+        radii = [] #A list of the radii
+        n = 1 #r0 = 0
+        while True:
+            rm = n * self.wavelength * np.sqrt(2 * Z/n/self.wavelength + n**2 / 4) #Calculated by pythagoras
+            if rm > self.size//2 or n>maxrings:
+                break #I want every ring to be completely inside my square
+            radii.append(rm)
+            n += 1
+
+        for m, rm in enumerate(radii):
+            self.slit += (np.sqrt((self.X - self.size//2)**2 + (self.Y - self.size//2)**2) < rm ) * (-1)**(m-1) * 0.5 + 0.5
 
     def propagate(self, Z, A):
         """
@@ -64,26 +84,26 @@ class Monochromatic_Experiment:
 
         Returns the forier transform A(kx, ky, Z)
         """
-        phase = np.exp(1j*Z*np.sqrt( (2*np.pi/self.wavelength)**2 - self.Kx**2 - self.Ky**2)) 
+        phase = np.exp(1j*Z*np.sqrt( (2*np.pi/self.wavelength)**2 - self.Kx**2 - self.Ky**2))  #Not mine :)
         return A*phase
 
     def fourier_transform(self):
         """Returns the fourier transform of the slit.
         """
         fourier = fft.fft2(self.slit)
-        return fft.fftshift(fourier)
+        return fft.fftshift(fourier) #necessary to get nice and tidy graphic results
 
     def intensity(self, F):
         """Returns the intensity of a field F
         Params:
         F: float
         """
-        return np.real(F*np.conjugate(F))
+        return np.real(F*np.conjugate(F)) #np real not to get a +0j
 
     def field_at_Z(self, Z):
         """Returns the interference pattern observed at a distance Z from the slit. Kind of a 'full experiment'
         Params:
-        Z: float
+        Z: float 
         Distance, in mm, from the slit to the 'screen'
         Returns:
         self.N x self.N array of intensities to be plotted"""
@@ -93,8 +113,9 @@ class Monochromatic_Experiment:
         #fourier_at_Z = fft.fftshift(fourier_at_Z) #Undo the shift
 
         field = fft.ifft2(fourier_at_Z)
-        
-        return self.intensity(field)
+
+        intensity = self.intensity(field)
+        return intensity/intensity.max()
 
     def evolve(self, Zmin, Zmax, N=100):
         """Returns a list of the fields for different Z"""
@@ -104,37 +125,51 @@ class Monochromatic_Experiment:
 
 #Units definition
 mm = 1
-m = 1000*mm
-cm = 10*m
+m = 1000 * mm
+cm = 10 * mm
 nm = 1e-6 * mm
 
-N = 200 # pixels number
-size = 10 * mm # physical length of a square (size_x = size_y)
+N = 700 # pixels number
+size = 20 * mm # physical length of the experiment (size_x = size_y)
 
-wavelength = 900*nm
+wavelength = 600*nm
 
-offset = 0.2 #offset of the slits. Half the distance between the centers of the slits.
-exp = Monochromatic_Experiment(wavelength, N, size, r0)
+exp = Monochromatic_Experiment(wavelength, N, size)
 
+"""Double slit experiment"""
+a = 1
+L = 2000
+offset = 0.2*mm #offset of the slits. Half the distance between the centers of the slits.
 #exp.grid(2, 100, N//2, N//2, 30, 20)
-exp.rectangle(0.1, 9, size//2, size//2 + offset)
-exp.rectangle(0.1, 9, size//2, size//2 - offset)
-#exp.rectangle(50, 50, N//2, N//2)
-exp.spherical_wave(20)
+#exp.rectangle(0.1, 9, size//2, size//2 + offset)
+#exp.rectangle(0.1, 9, size//2, size//2 - offset)
+#exp.rectangle(a, a, size//2, size//2)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(2, 1)
 
-ax.tick_params(left=False,
+ax[1].tick_params(left=False,
                 bottom=True,
                 labelleft=False,
                 labelbottom=True)
 
-ax.imshow(exp.field_at_Z(1000))
+#ax.imshow(exp.field_at_Z(1000))
+#field = exp.field_at_Z(L)
+#field0 = field[N//2, N//2]
+exp.fresnel_rings(1000, 10)
+#exp.spherical_wave(1000)
+ax[1].imshow(np.real(exp.slit))
+ax[0].set_title('Distance: ' +str(int(L)) + ' mm\nSide= ' + str(int(size)) + ' mm')
 
-for field, Z in exp.evolve(0, 1000, 300):  
-    ax.cla()
-    ax.set_title('Distance: ' +str(int(Z)) + ' mm\nSide= ' + str(int(size)) + ' mm')
-    exp.r0 += Z 
-    ax.imshow(field)
-    plt.pause(0.0000001)
+def animate(Z):
+    field = exp.field_at_Z(Z)
+    ax[0].cla()
+    ax[0].set_title('Distance: ' +str(int(Z)) + ' mm\nSide= ' + str(int(size)) + ' mm')
+    ax[0].plot(exp.x, field[:, N//2])
+
+    ax[1].cla()
+    ax[1].imshow(field)
+
+ani = FuncAnimation(fig, animate, frames=np.linspace(100, 200, 20), interval=1, repeat=False)
+
+
 plt.show()
